13bd78ddf6586635c92875f25ed4360a41372fb9
Jordan Isaacs <mail@jdisaacs.com>
2023-01-23 15:00:19 -0500

    buildExmap function

--- flake.nix
+++ flake.nix
@@ -10,34 +10,39 @@
 
     kernel = pkgs.linuxPackages_latest.kernel;
 
-    exmap = pkgs.stdenv.mkDerivation {
-      name = "exmap";
-      src = ./module;
-      outputs = ["out" "dev"];
+    buildExmap = kernel:
+      pkgs.stdenv.mkDerivation {
+        name = "exmap";
+        src = ./module;
+        outputs = ["out" "dev"];
 
-      buildInputs = [pkgs.nukeReferences];
-      kernel = kernel.dev;
-      kernelVersion = kernel.modDirVersion;
+        buildInputs = [pkgs.nukeReferences];
+        kernel = kernel.dev;
+        kernelVersion = kernel.modDirVersion;
 
-      buildPhase = ''
-        make -s "KDIR=$kernel/lib/modules/$kernelVersion/build" modules
-      '';
+        buildPhase = ''
+          make -s "KDIR=$kernel/lib/modules/$kernelVersion/build" modules
+        '';
 
-      installPhase = ''
-        mkdir -p $out/lib/modules/$kernelVersion/misc
-        for x in $(find . -name '*.ko'); do
-          nuke-refs $x
-          cp $x $out/lib/modules/$kernelVersion/misc/
-        done
+        installPhase = ''
+          mkdir -p $out/lib/modules/$kernelVersion/misc
+          for x in $(find . -name '*.ko'); do
+            nuke-refs $x
+            cp $x $out/lib/modules/$kernelVersion/misc/
+          done
 
-        mkdir -p $dev/include
-        cp -r linux $dev/include
-      '';
-    };
+          mkdir -p $dev/include
+          cp -r linux $dev/include
+        '';
+      };
+
+    exmap = buildExmap kernel;
   in {
     packages.x86_64-linux = {
       inherit exmap;
       default = exmap;
     };
+
+    lib = {inherit buildExmap;};
   };
 }
